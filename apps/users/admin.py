c7be21fe352e75cc87c7e7b7e8186f7619dc45c0
vash_ed <vash-ed@ya.ru>
2026-09-16 20:39:44 +0300

    chore(config): настройка settings, celery, базовых шаблонов и скелета модели users

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ("email", "username", "is_staff", "is_active")
+    search_fields = ("email", "username")
+    ordering = ("email",)
